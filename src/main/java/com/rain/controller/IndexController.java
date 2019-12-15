e9091dff1c1197389e2438dcb280c6d18e6dae79
whatarainyday <409144911@qq.com>
2019-12-15 17:51:42 +0800

    first commit

---
+++ src/main/java/com/rain/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.rain.controller;/*
+
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
